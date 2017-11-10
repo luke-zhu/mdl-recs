6b54ffa322240bd9004c09c4a3154544e4a2cd9a
luke-zhu <luke_zhu@brown.edu>
2017-11-10 01:44:50 -0500

    Migrated to React 16 and react-router v4

--- web/src/components/MDLCard.jsx
+++ web/src/components/MDLCard.jsx
@@ -1,17 +1,18 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import {
-  Card,
-  CardTitle,
+    Card,
+    CardTitle,
 } from 'material-ui/Card';
 
-const MDLCard = ({ show }) => (
-  <Card>
-    <CardTitle title={show.name} subtitle={show.myScore} />
-  </Card>
+const MDLCard = ({show}) => (
+    <Card>
+        <CardTitle title={show.name} subtitle={show.myScore}/>
+    </Card>
 );
 
 MDLCard.propTypes = {
-  show: PropTypes.object.isRequired,
+    show: PropTypes.object.isRequired,
 };
 
 export default MDLCard;
--- web/src/components/MDLInputView.jsx
+++ web/src/components/MDLInputView.jsx
@@ -1,51 +1,35 @@
-import React, { PropTypes } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 import TextField from 'material-ui/TextField';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 
 import {
-  showLoading,
-  getMyShows,
-  getSimilarShows
+    showLoading,
+    getMyShows,
+    getSimilarShows
 } from '../redux/actions';
 
-const MDLInputView = ({ handleKeyPress }) => (
-  <div>
-    <TextField
-      floatingLabelText="Enter your MDL username here."
-      onKeyPress={handleKeyPress}
-    />
-  </div>
+const MDLInputView = ({handleKeyPress}) => (
+    <div>
+        <TextField
+            floatingLabelText="Enter your MDL username here."
+            onKeyPress={handleKeyPress}
+        />
+    </div>
 );
 
 MDLInputView.propTypes = {
-  handleKeyPress: PropTypes.func.isRequired,
+    handleKeyPress: PropTypes.func.isRequired,
 };
 
 const mapDispatchToProps = dispatch => ({
-  handleKeyPress: (e) => {
-    if (e.key === 'Enter') {
-      console.log('Username entered');
-      dispatch(showLoading());
-      Meteor.call('mdl.getList', e.target.value, (error, result) => {
-        if (error) console.error(error);
-        else {
-          console.log(result);
-          dispatch(getMyShows(result));
-
-          Meteor.call('similarities.similarTo',
-            result.map(show => show.name),
-            (error2, result2) => {
-              if (error2) console.error(error2);
-              else {
-                console.log(result2);
-                dispatch(getSimilarShows(result2));
-              }
-            }
-          );
+    handleKeyPress: (e) => {
+        if (e.key === 'Enter') {
+            console.log('Username entered');
+            console.log('Migrating!')
+            dispatch(showLoading());
         }
-      });
-    }
-  },
+    },
 });
 
 export default connect(null, mapDispatchToProps)(MDLInputView);
