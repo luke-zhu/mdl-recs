c29bf98f8a9c583a5fc8a8dffccf7327dc9a7d41
luke-zhu <luke_zhu@brown.edu>
2018-01-07 06:54:18 -0800

    Added rec, review, and show indices to Elasticsearch.

--- dataloaders.py
+++ dataloaders.py
@@ -1,17 +1,30 @@
 import glob
 import json
-
 import os
+
 from elasticsearch import Elasticsearch
 
-if __name__ == '__main__':
-    es = Elasticsearch()
-    filepaths = glob.glob(os.path.join('data/show-1511337330/shows', '*'))
+
+def load_data(es, index, dirpath):
+    """Loads the jsonlines data from the given dirpath to
+    the given mapping in the mydramalist index.
+    """
+    filepaths = glob.glob(os.path.join(dirpath, '*'))
     for fp in filepaths:
         with open(fp) as f:
             for line in f:
-                print(line)
-                res = es.index(index='mydramalist', doc_type='show', body=json.loads(line))
-                print(res['created'])
-                break
-        break
+                item = json.loads(line)
+                es.index(index=index,
+                         doc_type=index,
+                         id=item['id'],
+                         body=item)
+
+
+if __name__ == '__main__':
+    es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+    load_data(es, 'show', 'data/show-1511337330/shows')
+    load_data(es, 'comment', 'data/show-1511337330/comments')
+    load_data(es, 'rec', 'data/show-1511337330/recs')
+    load_data(es, 'review', 'data/show-1511337330/reviews')
+
+
